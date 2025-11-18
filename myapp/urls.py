70571bcbb57be82d1385df03f91a748f4b6c8792
Keita Nakata <keita.prog823@gmail.com>
2025-11-18 16:48:38 +0900

    forgot to add this

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ApiTestView, RakutenRedirectView
+from .views import ApiTestView, RakutenRedirectView, RankingView
 
 urlpatterns = [
     path("api_test/", ApiTestView.as_view(), name="api_test"),
     path("go/rakuten/", RakutenRedirectView.as_view(), name="rakuten_redirect"),
+    path("ranking/", RankingView.as_view(), name="ranking"),
 ]
